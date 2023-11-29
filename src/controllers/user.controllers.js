602c6f9b70f69d900e1c08a3a4055882019272c9
Bruno <castro_bruno2006@hotmail.com>
2023-11-28 22:24:40 -0300

    Update user.controllers.js

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,10 +1,14 @@
 const create = (req, res) => {
 
-  const user = req.body;
+  const {name,username,email,passworld,avatar} = req.body;
+
+  if(!name || !username || !email || !passworld || !avatar){
+         
+    res.status(400).json('erro ao cadastrar')
+  }
 
 
-
-  res.json(user);
+  res.status(200).json('ok');
 };
 
 module.exports = { create };
